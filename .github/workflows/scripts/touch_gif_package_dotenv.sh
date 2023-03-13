8981bf1336e12435fb91461a2ea2134d6f1c2fa4
Marc Doerflinger <marc@doerf.li>
2023-03-13 07:45:07 +0000

    fix build

--- .github/workflows/scripts/touch_gif_package_dotenv.sh
+++ .github/workflows/scripts/touch_gif_package_dotenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-GIF_CONTRACTS_PKG_DIR=`find . ~/ -name brownie-config.yaml | grep gif-contracts | xargs dirname`
-touch $GIF_CONTRACTS_PKG_DIR/.env
+# GIF_CONTRACTS_PKG_DIR=`find . ~/ -name brownie-config.yaml | grep gif-contracts | xargs dirname`
+# touch $GIF_CONTRACTS_PKG_DIR/.env
 
 OZ_CONTRACTS_PKG_DIR=`find . ~/ -name brownie-config.yaml | grep openzeppelin-contracts | xargs dirname`
 touch $OZ_CONTRACTS_PKG_DIR/.env
