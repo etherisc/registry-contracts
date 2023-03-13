4db3bf773de071499d14b7497866b68fd560e713
Marc Doerflinger <marc@doerf.li>
2023-03-13 07:49:04 +0000

    dirty hack fix for build issue

--- .github/workflows/scripts/touch_gif_package_dotenv.sh
+++ .github/workflows/scripts/touch_gif_package_dotenv.sh
@@ -5,3 +5,4 @@
 
 OZ_CONTRACTS_PKG_DIR=`find . ~/ -name brownie-config.yaml | grep openzeppelin-contracts | xargs dirname`
 touch $OZ_CONTRACTS_PKG_DIR/.env
+touch /home/runner/.brownie/packages/OpenZeppelin/openzeppelin-contracts@4.8.2/.env
